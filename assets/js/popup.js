0912e7a0957a36b90fd3f1561ca878f289e38896
katiem120598 <katiem120598@gmail.com>
2025-11-27 12:30:04 -0500

    Updated nova apps and refactored a bit

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const title = item.dataset.projectTitle || "project";
         const desc  = item.dataset.projectDesc  || "";
         const link  = item.dataset.projectLink  || null;
-        const playlink  = item.dataset.projectPlaylink  || null;
+        const linktitle = item.dataset.projectLinktitle || null;
 
         // 🔹 skills now defined *per item*
         const skills = item.dataset.projectSkills
@@ -74,14 +74,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     ${
                         link
                         ? `<a href="${link}" target="_blank" rel="noopener noreferrer">
-                               open full project →
-                           </a>`
-                        : ''
-                    }
-                    ${
-                        playlink
-                        ? `<a href="${playlink}" target="_blank" rel="noopener noreferrer">
-                               try it out !!! →
+                               ${linktitle}
                            </a>`
                         : ''
                     }
