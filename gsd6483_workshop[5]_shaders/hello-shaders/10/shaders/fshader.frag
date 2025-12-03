b401417cee57dc8e037591f7eb4598ce1f672425
katiem120598 <katiem120598@gmail.com>
2025-12-02 19:56:54 -0500

    removed unnecessary folders

--- gsd6483_workshop[5]_shaders/hello-shaders/10/shaders/fshader.frag
+++
@@ -1,9 +0,0 @@
-#ifdef GL_ES
-precision mediump float;
-#endif
-
-// Main fragment function
-void main() {
-    // Assign a vec4 value to gl_FragColor
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-}
--- voxel/dropdowntoggle.js
+++
@@ -1,8 +0,0 @@
-function toggleDropdown() {
-    var content = document.querySelector('.dropdown-content');
-    if (content.style.display === "flex") {
-        content.style.display = "none";
-    } else {
-        content.style.display = "flex"; // This makes the dropdown part of the document flow
-    }
-}
